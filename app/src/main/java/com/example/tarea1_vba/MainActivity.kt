35345491deaaea0458a943822af6449e676a8382
Alexis Vaquero <alexisvaquero@hotmail.com>
2022-11-20 22:27:18 -0600

    finish-activity2

--- app/src/main/java/com/example/tarea1_vba/MainActivity.kt
+++ app/src/main/java/com/example/tarea1_vba/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tarea1_vba
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -19,21 +20,57 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClick(view: View) {
-        //if (binding.btnEnter.isPressed){
-            if (tvIsNotEmpty()){
-                // se pasa a la otra activity
-                binding.tvTitle.text = "jajajaja"
-            } else {
-                Toast.makeText(this@MainActivity, getString(R.string.items_error),Toast.LENGTH_LONG).show()
-            }
-        //}
+        when(view.id){
+            R.id.btn_enter -> {
+                if (tvIsNotEmpty()){
+                    // se pasa a la otra activity
+                    val intent = Intent(this, MainActivity2::class.java)
 
-        if (binding.btnLanguage.isPressed){
-            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS));
+                    val parameters = Bundle()
+
+                    parameters.putString("name", binding.tvName.text.toString())
+                    parameters.putString("date", binding.tvDate.text.toString())
+                    parameters.putString("account", binding.tvNaccount.text.toString())
+                    parameters.putString("email", binding.tvEmail.text.toString())
+
+                    intent.putExtras(parameters)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@MainActivity, getString(R.string.items_error),
+                        Toast.LENGTH_LONG).show()
+                }
+            }
+
+            R.id.tv_date -> {
+                datePicker()
+                // EL resultado se encuentra en
+                //binding.tvDate.text
+            }
+
+            R.id.btn_language -> {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS));
+            }
         }
     }
 
-    fun tvIsNotEmpty():Boolean{
+    private fun datePicker(){
+        // Valores por defecto del DatePicker
+        val year = 2000
+        val month = 0
+        val day = 1
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { view, year1, monthOfYear, dayOfMonth ->
+                val dateChoice = (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year1)
+                binding.tvDate.setText(dateChoice)
+                //temp = dateChoice
+            }, year, month, day
+        )
+        datePickerDialog.show()
+    }
+
+    private fun tvIsNotEmpty():Boolean{
         val check = mutableListOf<Boolean>() // Para comprobar que haya texto
         with(binding){
             val idsTexts = listOf(tvName.text,tvDate.text,tvNaccount.text,tvEmail.text)
@@ -49,7 +86,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun idError(id: Int){
+    private fun idError(id: Int){
         when (id){
             0 -> {
                 binding.tvName.error = getString(R.string.nombre_error)
--- app/src/main/java/com/example/tarea1_vba/MainActivity2.kt
+++ app/src/main/java/com/example/tarea1_vba/MainActivity2.kt
@@ -2,10 +2,36 @@ package com.example.tarea1_vba
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tarea1_vba.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // se reciben los parametros
+        val bundle = intent.extras
+        // se guardan
+        val name = bundle?.getString("name","")
+        val date = bundle?.getString("date","")
+        val account = bundle?.getString("account","")
+        val email = bundle?.getString("email","")
+
+
+        with(binding){
+            tvName.text = name
+            tvAge.text = date
+            tvNaccount.text = account
+            tvEmail.text = email
+        }
+
+
+
+
     }
+
+
 }
